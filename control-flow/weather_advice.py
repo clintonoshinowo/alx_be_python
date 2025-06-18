e7ce877db315802b63f20bc7121bc3b4e30dcdfa
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-06-18 14:48:28 -0600

    Add weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,14 +3,14 @@
 # Prompt the user for weather input
 # .lower() is used to make the input case-insensitive,
 # so "Sunny", "SUNNY", or "sunny" will all be treated the same.
-weather_condition = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 # Provide clothing recommendations based on the user's input
-if weather_condition == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_condition == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_condition == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     # Handle unexpected input
