69edcd9868d554f595793ca532c7575c3590f67a
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-06-18 14:57:27 -0600

    Add weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -13,5 +13,4 @@ elif weather == "rainy":
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    # Handle unexpected input
     print("Sorry, I don't have recommendations for this weather.")
